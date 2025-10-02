6c31bb132b13c2fb0771f3b3bd8280832ef9c56f
yamenAl <yamen_alsharabi@hotmail.com>
2025-10-03 01:41:09 +0200

    Cards html #46
    I did add the first 2 cards, description links, and title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
+    import { RoadMap, ToolKit } from '$lib';
   
 </script>
-
 
 <style>
 
@@ -29,9 +29,54 @@
         </svg>
     </a>
   </section>
+  
+  <article class="roadmap-section">
+     <a href="/roadmap">
+          <RoadMap />
+        </a>
+    <h2>
+        Civic Social Media Roadmap
+    </h2>
+
+     <a href="/"          
+        class="inline-flex items-center gap-3 font-semibold text-[#0F4A5A] hover:underline focus:underline"
+        aria-label="Lees meer"> Ontdek
+        <svg width="22" height="22" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+            <path d="M5 12h14m0 0-5-5m5 5-5 5"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </a>
+    <p>
+        Een routekaart voor de ontwikkeling van sociale media die veilig en betrouwbaar, 
+        verantwoord en soeverein is.
+    </p>   
+  </article>
+  <hr>
 
 
+  <article class="roadmap-section">
+     <a href="/toolkit" >
+          <ToolKit />
+        </a>
+    <h2>
+        Design principles for civic social media
+    </h2>
 
+     <a href="/"          
+        class="inline-flex items-center gap-3 font-semibold text-[#0F4A5A] hover:underline focus:underline"
+        aria-label="Lees meer"> Discover  
+        <svg width="22" height="22" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+            <path d="M5 12h14m0 0-5-5m5 5-5 5"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </a>
+     <p>
+        Would you like to start designing responsible, safe, and sovereign social media yourself? 
+        Download our toolkit below! The Designing for Civic Social Media toolkit contains 55 cards, 
+        composed of community archetypes, design principles, and dimensions for civic social media, 
+        which can help you in the design of digital platforms based on public values.     
+    </p>    
+  </article>
 
 </main>
 
