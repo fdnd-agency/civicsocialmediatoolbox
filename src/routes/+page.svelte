42b4da1e0675ccff722aa5bda5eebace16de947d
Karima002 <karima.mouadi@hva.nl>
2025-10-03 00:45:21 +0200

    #41 HTML card-layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import ontwerpprincipes from '$lib/assets/ontwerpprincipes.png';
+    import community from '$lib/assets/communityarchetypes.png';
+</script>
+
+
+
+<!-- Card-based layout -->
+<main>
+
+    <div class="card-wrapper">
+        <section class="cardlayout-section">
+            <img class="card-image" src="{ontwerpprincipes}" alt="infographic met vijf ontwerpprincipes" width="400" height="300" />
+                
+            <article class="card-layout">
+                <h2>Ontwerpprincipes</h2>
+                <p>Tijdens de ontwikkeling van civic social media maken softwareontwikkelaars en ondersteunende organi- saties keuzes op het gebied van technologie, algo-ritmes, governance, benodigde functionaliteiten, de gebruikerservaring (UX design), visuele stijl (UI design), moderatie en redactie. Om tot weloverwogen keuzes te komen, kunnen ontwerpprincipes steun bieden bij de ontwikkeling van civic socialmedia-platforms.</p>
+                <p>Deze ontwerpprincipes zijn terug te voeren op een aantal kernwaarden en kunnen worden vertaald naar een aantal ontwerpdimensies. De ontwerpprincipes beschrijven de onderliggende waarden, in de ontwerpdimensies worden die vertaald naar een aantal concrete opgaven voor ontwerpers.</p>
+            </article>
+        </section>
+
+        <section class="cardlayout-section">
+            <img class="card-image" src="{community}" alt="infographic met vijf ontwerpprincipes" width="400" height="300" />
+                
+            <article class="card-layout">
+                <h2>Community archetypes</h2>
+                <p>Bij civic social media staan de behoeften van gemeenschappen centraal. Wat die behoeften precies zijn en welke functionaliteiten daarvoor nodig zijn verschilt per gemeenschap. Om hier beter grip op te krijgen onderscheiden we zes archetypen. Ze overlappen elkaar deels, maar bij elkaar geven ze een beeld van de soorten communities die kunnen floreren op civic socialmediaplatforms.</p>
+                <p>Het doel van de archetypen is niet om te stellen dat er slechts zes soorten gemeenschappen of zes soorten civic social media bestaan, maar om handvatten te geven tijdens het ontwerpproces.</p>
+            </article>
+        </section>
+    </div>
+</main>
