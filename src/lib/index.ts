924bef26cbea0cc069d862cbc5df782aed10daa1
yamenAl <yamen_alsharabi@hotmail.com>
2025-10-03 01:42:20 +0200

    Cards svelte files #46
    I added the cards SVG as Svelte file and called them in the index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as RoadMap} from './assets/roadmap-icon.svelte';
+export { default as ToolKit } from './assets/toolkit-icons.svelte';
+
