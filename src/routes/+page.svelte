398627db706471bc39ea6fb510a751eedb9aafaa
Abeeryu <144008500+Abeeryu@users.noreply.github.com>
2025-10-05 23:58:52 +0200

    header
    header #38

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<header class="header">
+    <div class="header-text">
+        <h1>CIVIC INTERACTION DESIGN</h1>
+        <p>RESEARCH GROUP</p>
+    </div>
+    <img
+        class="logo" src="/logo-of-amsterdam.png" alt="logo" width="500" height="100"
+    />
+</header>
+
+<style>
+    header {
+        background-color: #ebdac5;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 15px;
+        padding: 10px 20px;
+    }
+
+    h1 {
+        font-size: 15px;
+        margin: 0;
+    }
+
+    p {
+        font-size: 10px;
+        margin: 0;
+    }
+
+    .logo {
+        width: 200px;
+        height: auto;
+    }
+</style>
