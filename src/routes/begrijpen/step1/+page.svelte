e20e245ab1b2f22498b82525b8aa22cc6b8d7338
Karima002 <karima.mouadi@hva.nl>
2025-10-25 00:43:40 +0200

    #99 CSS voor de pagina toegevoegd

--- src/routes/begrijpen/step1/+page.svelte
+++ src/routes/begrijpen/step1/+page.svelte
@@ -20,5 +20,41 @@
         <label>
             <input type="text" required placeholder="Geef uw antwoord..">
         </label>
+
+        <button  id="submit-button">Submit</button>
     </form>
 </main>
+
+<style>
+    main {
+        padding: 2em;
+    }
+
+    input {
+        width: 100%;
+        height: 10em;
+        padding: .5em;
+    }
+
+    form {
+        position: relative;
+        margin-top: 3em;
+    }
+
+    #submit-button {
+        all: unset;
+        background-color: var(--primary-color-deep-blue);
+        color: var(--neutral-color-white);
+        padding: .7em 2em .7em 2em;
+        border-radius: var(--radius-1);
+        display: flex;
+        position: absolute;
+        right: 0;
+        margin-top: .75em;
+        &:hover {
+            cursor: pointer;
+            text-decoration: underline;
+        }
+    }
+    
+</style>
