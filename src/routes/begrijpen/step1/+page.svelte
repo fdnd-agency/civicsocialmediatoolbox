c79e4c2882cc4e0f4ad49fb4c66d5d55fe41fd56
Karima002 <karima.mouadi@hva.nl>
2025-10-25 00:01:41 +0200

    #99 form toegevoegd

--- src/routes/begrijpen/step1/+page.svelte
+++ src/routes/begrijpen/step1/+page.svelte
@@ -1,10 +1,24 @@
 <script>
-    import Header from '$lib/components/Header.svelte'; 
+    import Header from '$lib/components/Header.svelte';
+    import StepOne from '$lib/assets/step-1.svelte';
+ 
 </script>
 
 
 <Header /> 
 
 <main>
-    
+    <h1>1. Begrijpen</h1>
+
+    <p>Dit is de eerste activiteit van de toolkit: Volg de stappen om naar de tweede activiteit te gaan.</p>
+
+    <StepOne />
+
+    <p>Begin met een onderzoeksvraag. Welke gemeenschap wil je beter begrijpen?</p>
+
+    <form>
+        <label>
+            <input type="text" required placeholder="Geef uw antwoord..">
+        </label>
+    </form>
 </main>
