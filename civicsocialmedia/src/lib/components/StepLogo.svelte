8f4cdd4918217e88cbc5d72d23c4efc0dfd2e5a2
Yamen <62349907+yamenAl@users.noreply.github.com>
2025-10-26 06:38:43 +0100

    adding progress bar logo #102
    Here I add the progress bar logo and change the names off the old icons

---
+++ civicsocialmedia/src/lib/components/StepLogo.svelte
@@ -0,0 +1,4 @@
+<svg width="167" height="162" viewBox="0 0 167 162" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="167" height="162" fill="#6097F9"/>
+<rect x="47" y="45" width="72" height="70" fill="white"/>
+</svg>
