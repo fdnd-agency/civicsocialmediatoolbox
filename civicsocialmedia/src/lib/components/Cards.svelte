e2e4a7fffee5f1b6e4e40573e94aae86c262c961
Karima002 <karima.mouadi@hva.nl>
2025-11-17 23:49:52 +0100

    #126 Cards component for displaying card list

---
+++ civicsocialmedia/src/lib/components/Cards.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Card from "./Card.svelte";
+    export let cards;
+    
+</script>
+
+<section class="card-container">
+
+    <ul>
+        {#each cards as card}
+            <li class="card">
+                <Card {card} />
+            </li>
+        {/each}
+    </ul>
+</section>
+
+<style>
+
+
+ul {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    }   
+
+
+
+
+</style>
