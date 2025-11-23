2c55f4f381114706b3596718258ff2148dd5cd89
Karima002 <karima.mouadi@hva.nl>
2025-11-23 17:13:43 +0100

    #126 Added HTML for the component

--- civicsocialmedia/src/routes/begijpen/+page.svelte
+++ civicsocialmedia/src/routes/begijpen/+page.svelte
@@ -1,10 +1,40 @@
 <script>
 
     import Cards from '$lib/components/Cards.svelte';
-
+    import ProgressStepper from '$lib/components/ProgressStepper.svelte';
     let { data } = $props();
 
+    const steps = [
+        { href: '/begijpen/step1' },
+        { href: '/begijpen/step2' },
+        { href: '/begijpen/step3' },
+    ];
+    const current = 1; // we are on step 2
     const cards = data.cards;
 </script>
 
-<Cards cards={data.cards} />
+<main>
+
+    <h2>1. Begrijpen</h2>
+
+    <ProgressStepper {steps} {current} />
+    <p>Verken de 6 community archetypes en de bijbehorende behoeften van communities en organisaties, daarna  kies 1 of 2 archetypes die het beste passen bij jou community en/of organisatie.</p>
+    <Cards cards={data.cards} />
+
+</main>
+
+<style>
+    main {
+        padding: 3em;
+    }
+
+    h2{
+        text-align: left;
+        padding-left: .5em;
+    }
+    
+    p{
+        padding-left: 1em;
+    }
+
+</style>
--- civicsocialmedia/src/routes/begijpen/step2/+page.svelte
+++ civicsocialmedia/src/routes/begijpen/step2/+page.svelte
@@ -1,11 +1,17 @@
 <script>
   import ProgressStepper from '$lib/components/ProgressStepper.svelte';
+  import Cards from '$lib/components/Cards.svelte';
+
   const steps = [
     { href: '/begijpen/step1' },
     { href: '/begijpen/step2' },
     { href: '/begijpen/step3' },
   ];
   const current = 1; // we are on step 2
+  const cards = data.cards;
+  let { data } = $props();
+
 </script>
 
+<Cards cards={data.cards} />
 <ProgressStepper {steps} {current} />
