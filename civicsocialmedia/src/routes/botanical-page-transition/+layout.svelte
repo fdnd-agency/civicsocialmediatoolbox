53b4f5186226093f369a746e8d0b682c89729ddd
Karima002 <karima.mouadi@hva.nl>
2025-12-04 23:45:33 +0100

    #173 Added view transition to sveltekit

---
+++ civicsocialmedia/src/routes/botanical-page-transition/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+import { onNavigate } from '$app/navigation';
+
+onNavigate((navigation) => {
+	if (!document.startViewTransition) return;
+
+	return new Promise((resolve) => {
+		document.startViewTransition(async () => {
+			resolve();
+			await navigation.complete;
+		});
+	});
+});
+
+let { children } = $props();
+
+</script>
+
+<main>
+	{@render children?.()}
+</main>
