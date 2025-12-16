e1a709db52e2ddcefdfba2d878f685085d8b1a45
Karima002 <karima.mouadi@hva.nl>
2025-12-16 11:24:25 +0100

    #194 added HTML to the overview page

---
+++ civicsocialmedia/src/routes/begrijpen/overzicht/+page.svelte
@@ -0,0 +1,32 @@
+<main>
+    <section>
+        <h2>Overzicht van alle vragen</h2>
+        <button class="print">Printen</button>
+    </section>
+
+    <label>Stap 1
+        <textarea readonly>Begin met een onderzoeksvraag. Welke gemeenschap wil je beter begrijpen? 
+        </textarea>
+    </label>
+
+    <label>Stap 2
+        <textarea readonly>Verken de 6 community archetypes en de bijbehorende behoeften van communities en organisaties, daarna kies 1 of 2 archetypes die het beste passen bij jouw community en/ of organisatie.
+        </textarea>
+    </label>
+
+    <label>Stap 3
+        <textarea readonly>Welke behoeften van communities en organisaties herken je? Op welke manieren is jouw community en/of organisatie anders?
+            Welke aannames maak je over jouw community? Zijn er blinde vlekken? Hoe zou je daar achter kunnen komen?
+        </textarea>
+    </label>
+
+    <ul>
+        <li>
+            <button>Bevestigen en verzenden</button>
+        </li>
+
+        <li>
+            <button>Volgende activiteit: Ontwerpen</button>
+        </li>
+    </ul>
+</main>
