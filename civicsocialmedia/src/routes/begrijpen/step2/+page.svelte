1f0d40a75dbb5af9327858b10d6512f9f50db7e5
Karima002 <karima.mouadi@hva.nl>
2025-12-16 10:12:21 +0100

    #193 changed h1 and p static

--- civicsocialmedia/src/routes/begrijpen/step2/+page.svelte
+++ civicsocialmedia/src/routes/begrijpen/step2/+page.svelte
@@ -1,31 +1,38 @@
 <script>
   import ProgressStepperV2 from '$lib/components/ProgressStepperV2.svelte';
+  import Cards from '$lib/components/Cards.svelte';
+
   export let data;
+
   const toolkitItems = data.toolkitItems ?? [];
+  const cards = data.cards;
 
   const currentStep = 2; // the current step number the user on it
   const toolkitItem = toolkitItems.find(i => Number(i.id) === currentStep) ?? null;
+  
 </script>
+
 <main>
-<ProgressStepperV2 {currentStep} />
 
-{#if toolkitItem}
-  <h1>1. {toolkitItem.title}</h1>
-  <p>{toolkitItem.subtitle}</p>
-{:else}
-  <p>Geen data gevonden voor deze stap.</p>
-{/if}
-
+  <ProgressStepperV2 {currentStep} />
+    <h2>1. Begrijpen</h2>
+    <p>Verken de 6 community archetypes en de bijbehorende behoeften van communities en organisaties, daarna  kies 1 of 2 archetypes die het beste passen bij jou community en/of organisatie.</p>
+  <Cards cards={data.cards} />
   
 </main>
 
 <style>
-    main {
-        padding: 2em;
+   main {
+        padding: 3em;
     }
 
-    h1{
+    h2{
+        text-align: left;
         padding-left: .5em;
+    }
+    
+    p{
+        padding-left: 1em;
     }
     
 </style>
