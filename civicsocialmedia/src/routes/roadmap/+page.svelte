5f062d3fd6cf903906fc906733695f7a1c727b2d
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-01-16 03:03:33 +0100

    static html code #163
    one column layout html static code adding the button for the pyramid

---
+++ civicsocialmedia/src/routes/roadmap/+page.svelte
@@ -0,0 +1,23 @@
+<main class="roadmap-main">
+  <h1>Roadmap</h1>
+
+  <section class="pyramid">
+    <button class="btn theme-layer-4">
+      Technologische Infrastructuur
+    </button>
+    <h2 class="label label-systemen">Systemen</h2>
+
+    <button class="btn theme-layer-3">
+      Wetgeving en Beleid
+    </button>
+
+    <button class="btn theme-layer-2">
+      Storytelling
+    </button>
+    <h2 class="label label-cultuur">Cultuur</h2>
+
+    <button class="btn theme-layer-1">
+      Waardenkader Media en Democratie
+    </button>
+  </section>
+</main>
