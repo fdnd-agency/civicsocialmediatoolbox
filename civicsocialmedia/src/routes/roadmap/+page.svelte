e52588212f198006fd0f385c46a38ae3477d0a21
Yamen <62349907+yamenAl@users.noreply.github.com>
2026-01-16 03:40:46 +0100

    adding style to the roadmap #163
    here im adding the css i desing this with grind so here i add the css that need it for this page and with hover en active stats and adding them for each layer in css

--- civicsocialmedia/src/routes/roadmap/+page.svelte
+++ civicsocialmedia/src/routes/roadmap/+page.svelte
@@ -1,6 +1,8 @@
 <main class="roadmap-main">
-  <h1>Roadmap</h1>
-
+  <h1>Civic Social Media Roadmap</h1>
+  <h3>Voor de ontwikkeling van civic social media moet er in uiteenlopende domeinen actie
+    worden ondernomen.</h3>
+  <p>Hoe creëren we een omgeving waarin civic social media kunnen floreren?</p>
   <section class="pyramid">
     <button class="btn theme-layer-4">
       Technologische Infrastructuur
@@ -21,3 +23,96 @@
     </button>
   </section>
 </main>
+
+<style>
+  :root {
+    --pyramid-clip: polygon(5% 0, 95% 0, 100% 100%, 0% 100%);
+  }
+
+  .roadmap-main {
+    max-width: 60rem;
+    margin: 0 auto;
+    padding: 2rem 1.5rem;
+  }
+
+ 
+
+  .pyramid {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    gap: 0.2rem;
+    max-width: 50rem;
+    justify-items: center;
+  }
+
+  .btn {
+    grid-column: 1 / 11;
+    padding: 1rem 1rem;
+    color: var(--neutral-color-white);
+    border: none;
+    cursor: pointer;
+    font-size: 0.75rem;
+    text-transform: uppercase;
+    letter-spacing: 0.05em;
+    transition: all 0.2s;
+    text-align: center;
+    border-radius: 0.5rem;
+    clip-path: var(--pyramid-clip);
+    
+    &:hover {
+      background-color: var(--secondary-color-soft-blue);
+      transform: translateY(-3px);
+    }
+
+    &:active {
+      transform: translateY(2px);
+    }
+  }
+
+  .label {
+    grid-column: 11 / 13;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    color: var(--font-color);
+    font-weight: 800;
+    font-style: bold;
+  }
+
+  .label-systemen {
+    grid-row: 1 / 3;
+  }
+
+  .label-cultuur {
+    grid-row: 3 / 5;
+  }
+
+  .theme-layer-1 {
+    width: 100%;
+    max-width: 600px;
+    background-color: var(--first-layer-color);
+  }
+
+  .theme-layer-2 {
+    width: 89%;
+    max-width: 530px;
+    margin-top: 10px;
+    background-color: var(--first-layer-color);
+  }
+
+  .theme-layer-3 {
+    width: 78%;
+    max-width: 460px;
+    background-color: var( --second-layer-color);
+  }
+
+  .theme-layer-4 {
+    width: 69%;
+    max-width: 400px;
+    background-color: var(--third-layer-color);
+
+  }
+
+  
+</style>
