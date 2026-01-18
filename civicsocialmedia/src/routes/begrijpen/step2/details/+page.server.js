24f4dd50a20cc247534ae3249d8ed0b09a11d014
Karima002 <karima.mouadi@hva.nl>
2026-01-18 21:20:27 +0100

    #123 #202 added server.js for detailed page

---
+++ civicsocialmedia/src/routes/begrijpen/step2/details/+page.server.js
@@ -0,0 +1,17 @@
+export async function load({ url }) {
+    
+    const API = 'https://fdnd-agency.directus.app/items/Card';
+    const fields = "?fields=id,title,categorie,posterimage,body,subtitle";
+    const filter = "&filter[id][_in]=16,17,18";
+
+
+    // Converting to JSON
+    const cardResponse = await fetch(API + fields + filter);
+    const cardData = await cardResponse.json();
+
+    console.log(cardData);
+
+    return {
+        cards: cardData.data,
+    };
+}
