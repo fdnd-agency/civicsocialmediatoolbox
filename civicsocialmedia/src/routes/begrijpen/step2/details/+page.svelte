ab096eb80a4a4335cc6d11ce8520184af09817ab
Karima002 <karima.mouadi@hva.nl>
2026-01-20 16:08:51 +0100

    #123 #202 added new page after clicking on "lees meer" step 2

---
+++ civicsocialmedia/src/routes/begrijpen/step2/details/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+  import ProgressStepperV2 from '$lib/components/ProgressStepperV2.svelte';
+  import DetailCards from '$lib/components/DetailCards.svelte';
+
+  export let data;
+
+  const toolkitItems = data.toolkitItems ?? [];
+  const cards = data.cards;
+
+  const currentStep = 2; // the current step number the user on it
+  const toolkitItem = toolkitItems.find(i => Number(i.id) === currentStep) ?? null;
+  
+</script>
+
+<main>
+
+  <ProgressStepperV2 {currentStep} />
+    <h2>1. Begrijpen</h2>
+    <p>Verken de 6 community archetypes en de bijbehorende behoeften van communities en organisaties, daarna  kies 1 of 2 archetypes die het beste passen bij jou community en/of organisatie.</p>
+  <DetailCards cards={data.cards} />
+  
+</main>
+
+<style>
+   main {
+        padding: 3em;
+    }
+
+    h2{
+        text-align: left;
+        padding-left: .5em;
+    }
+    
+    p{
+        padding-left: 1em;
+    }
+/* 
+    @media (prefers-color-scheme: dark) {
+        main {
+            background-color: var(--neutral-color-black);
+        }
+    }  */
+    
+</style>
+
