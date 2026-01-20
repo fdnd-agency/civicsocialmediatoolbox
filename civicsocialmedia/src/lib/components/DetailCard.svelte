779416a70facff338ac04bdb1ecc22d129006655
Karima002 <karima.mouadi@hva.nl>
2026-01-20 16:08:10 +0100

    #123 #202 added detail card component for step 2 begrijpen

---
+++ civicsocialmedia/src/lib/components/DetailCard.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let card;
+    const imgUrl = `https://fdnd-agency.directus.app/assets/${card.posterimage}`;
+</script>
+
+<section class="card">
+    <p class="body">{@html card.body}</p>
+</section>
+
+<style>
+    
+.card {
+    width: 18.5em;
+    height: 26.25em;
+    margin: 1em;
+    border: var(--accent-color-teal) 1em solid;
+}
+
+h2 {
+    text-align: center;
+}
+</style>
