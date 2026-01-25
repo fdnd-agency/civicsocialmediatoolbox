7d6de14b1868f63ebb08247de7609066901a5dde
Karima002 <karima.mouadi@hva.nl>
2026-01-25 14:36:35 +0100

    #184 removed font for performance

--- civicsocialmedia/src/lib/components/Header.svelte
+++ civicsocialmedia/src/lib/components/Header.svelte
@@ -8,10 +8,6 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-	<link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap" rel="stylesheet">
-</svelte:head>
 
 <header class="header">
 	<a class="header-left" href="/">
